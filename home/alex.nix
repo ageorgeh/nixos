1b8926a9f071c7de265326eae3a1229155d409b0
Alexander Hornung <aghornung@gmail.com>
2025-04-25 16:14:59 +1000

    SSH config

--- home/alex.nix
+++ home/alex.nix
@@ -41,19 +41,34 @@
 
   programs.ssh = {
     enable = true;
+    forwardAgent = false;
+    hashKnownHosts = true;
+    serverAliveInterval = 60;
+    controlMaster = "auto";
+    controlPath = "~/.ssh/master-%r@%h:%p";
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/github_id";
+        identitiesOnly = true;
+      };
+    };
+
     extraConfig = ''
       AddKeysToAgent yes
-
-      Host github.com
-        IdentityFile ~/.ssh/github_id
-        IdentitiesOnly yes
-
-      Host 192.168.*
-        ForwardAgent yes
-        IdentityFile ~/.ssh/github_id
-        IdentitiesOnly yes
+      IdentitiesOnly yes
     '';
   };
+
+  home.file.".ssh/config" = {
+    target = ".ssh/config_source";
+    onChange = ''
+      install -m 400 ~/.ssh/config_source ~/.ssh/config
+    '';
+  };
+
 
   systemd.user.services.rclone-gdrive = {
     Unit = {
