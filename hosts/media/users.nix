c73f6a4954ba299006ab95667ea6c9934792a7e6
Alexander Hornung <aghornung@gmail.com>
2026-01-04 12:06:32 +1100

    qbittorrent user

--- hosts/media/users.nix
+++ hosts/media/users.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   users.users = {
@@ -9,9 +9,13 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOR+Q5C3CCcWO96jb3K5y8zepC01WfnrLvn9uzhHGMG8 media"
       ];
     };
-    qbittorrent = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
+    users.users.qbittorrent = {
+      isSystemUser = true;
+      group = "qbittorrent";
+      home = "/var/lib/qbittorrent";
+      createHome = true;
+      shell = pkgs.runCommand "nologin" { } "ln -s /sbin/nologin $out";
     };
+    users.groups.qbittorrent = { };
   };
 }
